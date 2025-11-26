567a3454ab1675592c338bd61719927ab7e2a44d
pianist1900 <iamwolfboy@163.com>
2025-11-26 21:49:16 +0800

    rebuilding site Wed Nov 26 21:49:16 CST 2025

--- notes/test.py
+++
@@ -1,6 +0,0 @@
-new_file = r'/Users/gong/Documents/PKU/Courses/python_basic/test_file_backup_new.txt'
-f = open(new_file, "w")
-f.write("Now you can add a new row")
-# f.close()
-f = open(new_file, "r")
-print(f.read())
